287475e5e2c1460cff185252dbd9207e0a2fd9f6
itsAkky <akkyintellero@.com>
2024-07-24 14:11:48 +0530

    trello removed from tool

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
-import { FaWordpress, FaTrello, FaGoogleDrive, FaDropbox, FaSlack, FaBuffer, FaEvernote } from 'react-icons/fa';
+import { FaWordpress, FaGoogleDrive, FaDropbox, FaSlack, FaBuffer, FaEvernote } from 'react-icons/fa';
 import "../../../src/Toolsstack.css";
 import Grum from '../../Assets/grum.png';
 
@@ -8,7 +8,7 @@ import Grum from '../../Assets/grum.png';
 const Toolsstack = () => {
   const tools = [
     { icon: <FaWordpress />, text: 'WordPress', url: 'https://wordpress.org/' },
-    { icon: <FaTrello />, text: 'Trello', url: 'https://trello.com/' },
+    //{ icon: <FaTrello />, text: 'Trello', url: 'https://trello.com/' },
     { icon: <FaGoogleDrive />, text: 'Google Drive', url: 'https://drive.google.com/' },
     { icon: <FaDropbox />, text: 'Dropbox', url: 'https://www.dropbox.com/' },
     { icon: <FaSlack />, text: 'Slack', url: 'https://slack.com/' },
@@ -18,6 +18,14 @@ const Toolsstack = () => {
 
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <a href="https://grum.co/" target="_blank" style={{ textDecoration: 'none', color: 'inherit' }}>
+          <div className="tool-container">
+            <img src={Grum}className="img-fluid" alt="avatar" width={"100px"} height={"100px"}/>
+            <span className="tool-text">Grum</span>
+          </div>
+        </a>
+      </Col>
       {tools.map((tool, index) => (
         <Col key={index} xs={4} md={2} className="tech-icons">
           <a href={tool.url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
@@ -28,16 +36,6 @@ const Toolsstack = () => {
           </a>
         </Col>
       ))}
-      
-      <Col xs={4} md={2} className="tech-icons">
-        <a href="https://grum.co/" target="_blank" style={{ textDecoration: 'none', color: 'inherit' }}>
-          <div className="tool-container">
-            <img src={Grum}className="img-fluid" alt="avatar" width={"100px"} height={"100px"}/>
-            <span className="tool-text">Grum</span>
-          </div>
-        </a>
-      </Col>
-
       <Col xs={4} md={2} className="tech-icons">
         <a href="https://thebacklinksagency.com/" target="_blank" style={{ textDecoration: 'none', color: 'inherit' }}>
           <div className="tool-container">
